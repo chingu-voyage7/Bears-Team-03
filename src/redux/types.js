601b823f9b17246a0259ecabff21f53376e969c9
R-Layer <layer@europe.com>
2018-11-29 14:53:15 +0100

    add redux types

---
+++ src/redux/types.js
@@ -0,0 +1,8 @@
+export const fetchUsers = {
+  REQUEST: 'FETCH_USERS_REQUEST',
+  SUCCESS: 'FETCH_USERS_SUCCESS',
+  FAILURE: 'FETCH_USERS_FAILURE',
+};
+
+export const generalFailure = 'GENERAL_FAILURE';
+export const logout = 'LOGOUT';
