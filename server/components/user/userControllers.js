58c952ea957eaa0b1957b2d1cd2fbe602da6d115
R-Layer <layer@europe.com>
2018-11-29 18:31:52 +0100

    add few general user routes

--- server/components/user/userControllers.js
+++ server/components/user/userControllers.js
@@ -1,5 +1,40 @@
+const User = require('./userModel');
 // These are the functions executed when you reach the requested endpoint
 
 exports.userTestRoute = (req, res) => {
   res.status(200).json({ message: 'You reached the test route, gj!' });
+};
+
+exports.userGetAll = (req, res) => {
+  User.find({})
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({ fail: err }));
+};
+
+exports.userGetByID = (req, res) => {
+  User.find({ _id: req.userData.id })
+    .then((user) => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ fail: { message: 'User not found!' } });
+      }
+    })
+    .catch(err => res.status(500).json({ fail: err }));
+};
+
+exports.userUpdateByID = (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.userData.id },
+    { $set: req.body },
+    { new: true, runValidators: true },
+  )
+    .then(updatedUser => res.status(200).json(updatedUser))
+    .catch(err => res.status(500).json({ fail: err }));
+};
+
+exports.userDeleteByID = (req, res) => {
+  User.findOneAndDelete({ _id: req.userData.id })
+    .then(() => res.status(204).end())
+    .catch(err => res.status(500).json({ fail: err }));
 };
--- server/components/user/userRoutes.js
+++ server/components/user/userRoutes.js
@@ -3,8 +3,17 @@ const express = require('express');
 // Middleware-like object that allow to 'pack' routes based on components
 const router = express.Router();
 
-const { userTestRoute } = require('./userControllers');
+const {
+  userTestRoute,
+  userGetAll,
+  userGetByID,
+  userUpdateByID,
+  userDeleteByID,
+} = require('./userControllers');
 
 router.get('/users-test-route', userTestRoute);
-
+router.get('./user/get-all', userGetAll);
+router.get('./user/get-by-id', userGetByID);
+router.get('./user/update-by-id', userUpdateByID);
+router.get('./user/delete-by-id', userDeleteByID);
 module.exports = router;
