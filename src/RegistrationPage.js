c67c98e2eef792bb6acac53f524a53956a95f4a1
Nick Torbich <torbics.miklos@gmail.com>
2018-12-01 01:24:39 +0100

    Bound 'this' to handleChange

--- src/RegistrationPage.js
+++ src/RegistrationPage.js
@@ -15,6 +15,8 @@ function TextInput(props) {
 class RegistrationPage extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleChange = this.handleChange.bind(this);
     this.state = {
       user: {
         fullname: '',
