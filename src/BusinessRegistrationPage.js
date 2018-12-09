2e5d0f36943c752dbb3da4832606246449060c4b
younesbourakadi <younes.bourakadi@gmail.com>
2018-12-09 14:38:58 +0000

    created component

---
+++ src/BusinessRegistrationPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BusinessRegistrationPage extends Component {
+  render() {
+    return (
+          <div />
+    );
+  }
+}
+
+export default BusinessRegistrationPage;
