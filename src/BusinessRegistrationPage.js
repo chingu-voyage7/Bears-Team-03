e5031d02a55adc1c0b10e5fad99fec1c0a405d2a
younesbourakadi <younes.bourakadi@gmail.com>
2018-12-15 00:25:25 +0000

    add business city component

--- src/BusinessRegistrationPage.js
+++ src/BusinessRegistrationPage.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import { Button, Container, Form, FormGroup, Label, Input, FormFeedback, FormText } from 'reactstrap';
 
 class BusinessRegistrationPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      company: {
+        companyName: '',
+        fiscalCode: '',
+        localOfficeddress: '',
+        localOffice_country: '',
+        localOffice_phone: '',
+        mainHQ: '',
+        workFields: ''
+      }
+    }
+  }
+
   render() {
     return (
-          <div />
+      <Container className="BusinessRegistrationPage">
+        <Form className="form">
+          <FormGroup>
+            <Label for="city">City:</Label>
+            <Input
+              type="text"
+              name="city"
+              label="City"
+              placeholder="ExampleCity">
+            </Input>
+            <FormFeedback>
+              Please input your city.
+            </FormFeedback>
+          </FormGroup>
+        </Form>
+      </Container>
     );
   }
 }
