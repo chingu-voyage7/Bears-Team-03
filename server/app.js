26d82270c8c804e321a148733b0d4ff50e99e86e
Nick Torbich <torbics.miklos@gmail.com>
2018-12-17 01:13:38 +0100

    add projectRoutes to app.js

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const app = express();
 
 const userRoutes = require('./components/user/userRoutes');
 const businessRoutes = require('./components/business/businessRoutes');
+const projectRoutes = require('./components/project/projectRoutes');
 const authRoutes = require('./components/auth/authRoutes');
 
 // Standard middleware that convert incoming request data ( if formatted as json)
@@ -13,6 +14,7 @@ app.use(express.json());
 // Appending user routes using a common identifier ( ... /users/...)
 app.use('/user', userRoutes);
 app.use('/business', businessRoutes);
+app.use('/project', projectRoutes);
 app.use(authRoutes);
 
 // Main route for testing purpose
