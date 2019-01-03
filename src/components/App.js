1446fe43197a3fbbfdf703c468464f5d0c65f01c
Nick Torbich <torbics.miklos@gmail.com>
2019-01-04 00:54:01 +0100

    Refactor the navbar to use reactstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import {
- BrowserRouter as Router, Switch, Route, NavLink 
+  BrowserRouter as Router, Switch, Route,
 } from 'react-router-dom';
 
 import RegistrationPage from './RegistrationPage';
 import ProjectPage from './projectForm/ProjectPage';
 import Login from './Login';
-import Navbar from './navbar/Navbar';
+import Navigation from './navbar/Navbar';
 import ProtectedRoute from './ProtectedRoute';
 
 import './App.css';
 
-const App = ({registerUser, loginUser, logoutUser, createProject, auth}) => (
+const App = ({ registerUser, loginUser, logoutUser, createProject, auth }) => (
   <Router>
     <>
-      <Navbar>
-        <NavLink to="/login">Login</NavLink>
-        <NavLink to="/register">Register</NavLink>
-        <NavLink to="/create-project">Create Project</NavLink>
-        <button onClick={logoutUser}>Logout</button>
-      </Navbar>
+      <Navigation auth={auth} />
       <Switch>
         <Route path="/login" render={props => <Login {...props} login={loginUser} />} />
         <Route path="/register" render={props => <RegistrationPage {...props} register={registerUser} />} />
