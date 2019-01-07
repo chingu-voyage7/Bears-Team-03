fd454d265d697ec7f919b83a9305f5721b8c7ed7
Nick Torbich <torbics.miklos@gmail.com>
2019-01-08 00:23:57 +0100

    Configured setup for Heroku deployment

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -6,6 +7,9 @@ const userRoutes = require('./components/user/userRoutes');
 const businessRoutes = require('./components/business/businessRoutes');
 const projectRoutes = require('./components/project/projectRoutes');
 const authRoutes = require('./components/auth/authRoutes');
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Standard middleware that convert incoming request data ( if formatted as json)
 // and put it into the req.body property
@@ -20,6 +24,11 @@ app.use(authRoutes);
 // Main route for testing purpose
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Get our from my main route!' });
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 module.exports = app;
