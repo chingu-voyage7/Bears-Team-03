0c02c356e73a178534cd661bfc8725e27bb61cc3
Nick Torbich <torbics.miklos@gmail.com>
2019-01-23 10:56:02 +0100

    Add style to profile page
    clean up the dependencies

--- client/src/components/profilePage/ProfilePage.js
+++ client/src/components/profilePage/ProfilePage.js
@@ -1,28 +1,137 @@
 import React from 'react';
+import { Container, Row, Col, Card, CardBody, ListGroup, Nav, NavItem, NavLink, TabContent, TabPane, Table, Badge } from 'reactstrap';
+
+import ProfileListItem from '../ProfileListItem';
+
 import './profilePage.css';
 
 class ProfilePage extends React.Component {
-  _isMounted = false;
-
   state = {
-    currentUser: {}
+    activeTab: 'profile',
+    fullname: '',
+    email: '',
+    phone: '',
+    volunteerField: [],
+    zipCode: '',
+    city: '',
+    country: '',
+    stateOrProvince: '',
+    timeAvailability: ''
+  }
+
+  toggle = (tab) => {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
   }
 
   componentDidMount() {
-    this._isMounted = true;
-    this.props.fetchUser();
-  }
-
-  componentWillUnmount() {
-    this._isMounted = false;
+    if (!this.props.auth.currentUser) {
+      this.props.fetchUser();
+    } else {
+      const { fullname, email, phone, volunteerField, zipCode, city, country, stateOrProvince, timeAvailability } = this.props.auth.currentUser;
+      this.setState({
+        fullname, email, phone, volunteerField, zipCode, city, country, stateOrProvince, timeAvailability
+      });
+    }
   }
 
   render() {
-    console.log(this.props)
     return (
-      <div>
-        <h1>Profile page {this.props.auth.currentUser ? this.props.auth.currentUser.fullname : ''}</h1>
-      </div>
+      <Container>
+        <Row>
+          <Col lg="4">
+            <div className="profile-card shadow">
+              <Card>
+                <CardBody className="bg-primary text-center rounded-top">
+                  <div className="user-box">
+                    <img src="https://image.flaticon.com/icons/svg/149/149071.svg" alt="User profile image" />
+                  </div>
+                  <h5 className="mb-1 text-white">{this.state.fullname}</h5>
+                  <h6 className="text-light">Volunteer</h6>
+                </CardBody>
+                <CardBody>
+                  <ListGroup>
+                    <ProfileListItem
+                      title={'Phone Number'}
+                      data={this.state.phone}
+                      icon={'fa fa-phone-square'}
+                    />
+                    <ProfileListItem
+                      title={'Email Address'}
+                      data={this.state.email}
+                      icon={'fa fa-envelope'}
+                    />
+                    <ProfileListItem
+                      title={'City'}
+                      data={this.state.city}
+                      icon={'fa fa-address-card'}
+                    />
+                  </ListGroup>
+                </CardBody>
+              </Card>
+            </div>
+          </Col>
+          <Col lg="8">
+            <Card className="shadow">
+              <CardBody>
+                <Nav tabs>
+                  <NavItem>
+                    <NavLink onClick={() => { this.toggle('profile'); }}>
+                      Profile
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink onClick={() => { this.toggle('edit'); }}>
+                      Edit
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+                <TabContent activeTab={this.state.activeTab}>
+                  <TabPane tabId="profile">
+                    <Row>
+                      <Col sm="12">
+                        <h5 className="mb-3">User Profile</h5>
+                        <Row>
+                          <Col md="6">
+                            <h6>About</h6>
+                            <p>I love to volunteer ❤</p>
+                          </Col>
+                          <Col md="6">
+                            <h6>Global goals</h6>
+                            {this.state.volunteerField.map((field, index) => {
+                              return (<Badge key={index} color="dark">{field}</Badge>);
+                            })}
+                          </Col>
+                          <Col md="12">
+                            <h5 className="mt-2 mb-3"><span className="fa fa-clock-o ion-clock float-right"></span> Recent Activity</h5>
+                            <Table striped hover>
+                              <tbody>
+                                <tr>
+                                  <td>
+                                    <strong>You</strong> applied to ACME Project
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </Table>
+                          </Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </TabPane>
+                  <TabPane tabId="edit">
+                    <Row>
+                      {/* Here comes the edit tab */}
+                    </Row>
+                  </TabPane>
+                </TabContent>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
